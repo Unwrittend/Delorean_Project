6736ec9578ae1e629b671cf76338673b0de97835
Pranav Rao <prao2@scu.edu>
2021-02-20 09:52:07 -0800

    Add description comment to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 // Project DeLorean
 // JavaScript Document
 
+// This document contains basic functions for the website's interactive elements.
+
 // The population and MSOC inputs require constant factors so the sliders work properly
 // Sliders go from 1-100. Factors make it easier to manage the maximum and minimum values
 var popConstant = 1000;
