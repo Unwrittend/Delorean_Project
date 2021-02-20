c3ac1700b768b451e9323ad9b0d5d83f7ffaaf15
Pranav Rao <prao2@scu.edu>
2021-02-20 09:45:18 -0800

    Add jQuery UI time picker, change font, change some colors

--- scripts/app.js
+++ scripts/app.js
@@ -28,3 +28,48 @@ function hideOptIn() {
 function showOptIn() {
 	$("#optin-panel").removeClass("d-none");
 }
+
+// Setup for the jQuery UI Slider
+var slider = $("#slider");
+var hours1 = $("#hours-1");
+var hours2 = $("#hours-2");
+var hrs1, hrs2;
+
+slider.slider({
+	min: 0,
+	max: 24,
+	range: true,
+	values: [ 9, 17 ],
+
+	slide: function(event, ui){
+		hrs1 = ui.values[0];
+		hrs2 = ui.values[1];
+
+		if (hrs1 === 24) {
+			hrs1 = 0;
+		} else if (hrs1 >= 10) {
+			hours1.val( ui.values[0] + ":00" );
+		} else {
+			hours1.val( "0" + ui.values[0] + ":00" );
+		}
+
+		if (hrs2 === 24) {
+			hrs2 = 0;
+		} else if (hrs2 >= 10) {
+			hours2.val( ui.values[1] + ":00" );
+		} else {
+			hours2.val( "0" + ui.values[1] + ":00" );
+		}
+	}
+});
+
+hours1.val( "0" + slider.slider( "values", 0 ) + ":00" );
+hours2.val( slider.slider( "values", 1 ) + ":00" );
+
+hours1.change(function(){
+	slider.slider( "values", 0, hours1.val().substring(0, 1) );
+});
+
+hours2.change(function(){
+	slider.slider( "values", 1, hours2.val().substring(0, 1) );
+});
