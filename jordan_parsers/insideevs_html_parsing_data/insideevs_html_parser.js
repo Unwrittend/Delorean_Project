b3692c9c3cd20f21e621d0214d003afeddb09d8c
Jordan Randleman <jrandleman@scu.edu>
2021-02-20 15:22:57 -0800

    Update insideevs_html_parser.js

--- jordan_parsers/insideevs_html_parsing_data/insideevs_html_parser.js
+++ jordan_parsers/insideevs_html_parsing_data/insideevs_html_parser.js
@@ -31,17 +31,21 @@ function getTableRows($table) {
   return Object.values($table.children).filter(e => e.type == 'tag' && e.name == 'tr');
 }
 
+
 function getRowColumns($row) {
   return Object.values($row.children).filter(e => e.type == 'tag' && e.name == 'td');
 }
+
 
 function getRowModelName($columnsOfRow) {
   return $columnsOfRow[MODEL_NAME_IDX].children[0].children[0].data;
 }
 
+
 function getRowBatteryCapacity($columnsOfRow) {
   return parseFloat($columnsOfRow[BATTER_KWH_CAPACITY_IDX].children[0].data);
 }
+
 
 function getRowValuesObject($row) {
   const values = getRowColumns($row);
@@ -51,28 +55,39 @@ function getRowValuesObject($row) {
   };
 }
 
+
+function parseHtml(html) {
+  const $ = cheerio.load(html);
+  const data = [];
+  const table1Rows = getTableRows($('tbody')['1']);
+  const table2Rows = getTableRows($('tbody')['3']);
+  for(let r = 1; r < table1Rows.length; ++r)
+    data.push(getRowValuesObject(table1Rows[r]));
+  for(let r = 1; r < table2Rows.length; ++r)
+    data.push(getRowValuesObject(table2Rows[r]));
+  return data;
+}
+
 /******************************************************************************
 * MAIN DISPATCH
 ******************************************************************************/
+
+function print(obj) {
+  console.log(JSON.stringify(obj, null, 2));
+}
+
 
 function main() {
   fetch(SOURCE_URL, {method: 'get'})
     .then(response => response.text())
     .then(html => {
-      const $ = cheerio.load(html);
-      const data = [];
-      const table1Rows = getTableRows($('tbody')['1']);
-      const table2Rows = getTableRows($('tbody')['3']);
-      for(let r = 1; r < table1Rows.length; ++r)
-        data.push(getRowValuesObject(table1Rows[r]));
+      const data = parseHtml(html);
 
       ////////////////////////////////////////////////////////
       // ... CODE TO MANIPULATE/USE <data> HERE! ...
       ////////////////////////////////////////////////////////
 
-
-      console.log(JSON.stringify(data, null, 2));
-
+      print(data);
 
     }).catch(err => {
       console.error(`FATAL ERROR: ${err}`);
