a0eaf0337c3841f4458ca843cf33868b92af6567
Unwrittend <cpitterle@scu.edu>
2021-02-20 20:27:49 -0800

    update

--- scripts/fleet-capacity.js
+++ scripts/fleet-capacity.js
@@ -1,7 +1,10 @@
 // jQuery Document
 
 // Variables for 3 user inputs. Names self-explanatory. MSOC is Minimum State of Charge
-var vehicle_type, msoc, opt_in, battery_capacity;
+var vehicle_type, msoc, opt_in, battery_capacity, zip_code;
+
+//this is the average vehicles owned per household
+const average_veh_per_home = 1.85988258;
 
 // When multiple choice is changed, update variable value
 $("#mc .multi-choice .option").click(function(){
@@ -11,15 +14,18 @@ $("#mc .multi-choice .option").click(function(){
 
 // When MSOC inputs are changed, update variable value
 $("#msocText, #msocSlider").change(function(){
-	msoc = $("#msocText").val();
+	msoc = $("#msocText").val()				//is a percentage;
 	console.log(msoc);
 });
 
 // When Opt-in inputs are changed, update variable value
 $("#optinText, #optinSlider").change(function(){
-	opt_in = $("#optinText").val();
+	opt_in = $("#optinText").val()/100;		//is a percentage
 	console.log(opt_in);
 });
+
+//Retrieve zip code from map
+
 
 //Chris
 //TO DO make dynamic
@@ -47,5 +53,9 @@ run();
 
 //Determine the number of vehicles in the fleet
 //TO DO: use zip code -> populations
+//use zip code to determine population(pop)
+var veh_pop = pop * opt_in;
 
 
+//calculate fleet's overall capacity
+var flt_cap = veh_pop * msoc * battery_capacity;
