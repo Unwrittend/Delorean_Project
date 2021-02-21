c7c099e9e7597855392aa7921c40446b5551bf2f
Unwrittend <cpitterle@scu.edu>
2021-02-20 20:09:02 -0800

    battery capacity

--- scripts/fleet-capacity.js
+++ scripts/fleet-capacity.js
@@ -1,7 +1,7 @@
 // jQuery Document
 
 // Variables for 3 user inputs. Names self-explanatory. MSOC is Minimum State of Charge
-var vehicle_type, msoc, opt_in;
+var vehicle_type, msoc, opt_in, battery_capacity;
 
 // When multiple choice is changed, update variable value
 $("#mc .multi-choice .option").click(function(){
@@ -21,9 +21,31 @@ $("#optinText, #optinSlider").change(function(){
 	console.log(opt_in);
 });
 
+//Chris
+//TO DO make dynamic
 //retrieve vehicle statistics
-
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://chris:delorean@cluster0.joafk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('delorean');
+    const collection = database.collection('Vehicles');
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { model: vehicle_type };
+	const car = await collection.findOne(query);	  
+	//retrieved variables
+	battery_capacity = car.batteryKWHCapacity;
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run();
 
 //Determine the number of vehicles in the fleet
 //TO DO: use zip code -> populations
+
 
