993dde8da2c957d081285137c31a22cd12220155
Jordan Randleman <jordanran199@gmail.com>
2021-05-27 14:23:43 -0700

    Battery Degradation Incorporation

---
+++ public/scripts/battery-degredation.js
@@ -0,0 +1,160 @@
+//Author: Chris Pitterle
+//Created: 4/12/21
+//Last Update: 4/30/21
+
+/*
+Script's goal: Calculate EV Battery Degradation using Uddin's algorithms. 
+    Used equivalent circuit model (ECM) for CF and PF algorithms.
+Algorithms used:
+    - Capacity Fade: Know current and max capacity                                      --Uddin et al 2017
+    - Capacity Fade: Know MIN_SOC, tempurature, and ampere hours                        --Cordoba-Arenas et al. 2015
+    - Power Fade: Know current and max power fade                                       --Uddin et al 2017
+    - Power Fade: Know MIN_SOC, tempurature, and ampere hours                           --Cordoba-Arenas et al. 2015
+    - conversion to cost: max[CF x $_batt, PF x $_batt]                                 --Uddin et al 2017
+    - Battery Management System (BMS)                                                   --Uddin et al 2017
+*/
+
+/**
+ * compares PF and CF battery degradation and converts the worse of the 2's degradation into a monetary value
+ * 
+ * @param {*} batt_price = price of a replacement battery
+ * @param {*} CF = capacity fade
+ * @param {*} PF = power fade
+ * @returns 
+ */
+function convert_batt_degrad_to_dollar(batt_price, CF, PF) {
+    return Math.max(CF * batt_price, PF * batt_price)
+}
+
+/**
+ * Algorithm from Uddin et al. 2017
+ * Calculates the EV's current capacity fade if you know the max cap and current cap
+ * 
+ * Algorithm: CF = 1 - (cell_cap - CF_factor * max_rated_cell_cap) / (max_rated_cell_cap - CF_factor * max_rated_cell_cap);
+ * 
+ * @param {*} cell_cap - cell/battery capacity of vehicle 
+ * @param {*} CF_factor - the point at which a vehicle's battery is no longer fit for purpose 
+ *                          (often cited at 0.8 so this is the default) 
+ * @param {*} max_rated_cell_cap - manufacturer defined max capacity of a new, mint battery 
+ * @return Capacity Fade of EV battery
+ */
+function calc_cf_with_current_cap(cell_cap, max_rated_cell_cap, CF_factor = .8) {
+    return 1 - (cell_cap - CF_factor * max_rated_cell_cap) / (max_rated_cell_cap - CF_factor * max_rated_cell_cap);
+    //TODO: Needs to calc what cell cap is
+}
+
+/**
+ * @jordan use this
+ * Algorithm from Cordoba-Arenas et al. 2015
+ * Cycle aging algorithm
+ * calculates the capacity lose (%) of capacity fade under SOC, total Ampere hours sent, and Temp conditions
+ * 
+ * @param {*} SOC_min = the minimum SOC 
+ * @param {*} Ah = Ampere-hour throughput ie the total current to the battery over an hour 
+ *                      (do we control this? Let's say the CR [ie C] is 1C == 2A)
+ * @param {*} tempurature = tempurature of the cells in the vehicle (Unit: Kelvin)
+ * @param {*} time_cd = time in charge depleting mode (Hybrid only. If full EV, set to 1)
+ * @param {*} time_cs = time in charge sustain mode (Hybrid only. If full EV, set to 0)
+ * @returns Q_loss-cycle, a percentage of capacity lose
+ */
+ function calc_cf_cycle_aging_with_SOC_and_temp(SOC_min, Ah, tempurature, time_cd = 1, time_cs = 0) { // Used by <fleet-capacity,js>
+    //constants
+    var a_c = 137,
+        B_c = 420,
+        y_c = 9610,
+        b_c = 0.34,
+        c_c = 3,
+        E_a = 22406,    //Unit: J/mol
+        R = 8.314,      //Unit: J/(mol*K)
+        z = 0.48;
+    
+    var ratio = time_cd / (time_cd + time_cs);
+
+    return (a_c + (B_c * Math.pow(ratio, b_c)) + (y_c * Math.pow((SOC_min - 0.25), c_c))) * Math.exp((-E_a / (R * tempurature))) * Math.pow(Ah, z);
+ }
+
+ // Returns the Ampere-hour of the given model
+ //   => <voltOfChargingStation> is variable, BUT for our math can temporarily be 120V for a lvl1 charging station
+ //   => <kWhPerMile> & <milesDrivenPerYr> are Model-Specific
+ function get_ampHr(milesDrivenPerYr, kWhPerMile, voltOfChargingStation = 120) { // Used by <fleet-capacity,js>
+    const kWhPerYr = milesDrivenPerYr * kWhPerMile;
+   return 1000 * kWhPerYr / voltOfChargingStation;
+ }
+
+ /**
+  * Algorithm from Wang et al. 2014
+  * Calender aging algorithm
+  * calculates the capacity lose (%) of capacity fade over a period of days
+  * 
+  * @param {*} cell_temp - tempurature of the EV's battery cells
+  * @param {*} days - calendar time (unit: days)
+  * @returns - capacity fade over a period of days
+  */
+function calc_cf_calendar_aging(cell_temp, days) {
+    var A = 14786,
+        Ea = 24500,
+        R = 8.314;
+
+    return A * Math.exp(-Ea / (R * cell_temp)) * Math.sqrt(days);
+ }
+
+/**
+ * Algorithm from Uddin et al. 2017
+ * Calculates the power fade of a EV battery if you know the max resistance and current resistance
+ * 
+ * Algorithm: PF = (1 / (PF_factor - 1)) * (((internal_res + charge_transfer_res) / (init_charge_transfer_res + init_internal_res)) - 1);
+ *
+ * @param {*} internal_res 
+ * @param {*} charge_transfer_res 
+ * @param {*} init_internal_res 
+ * @param {*} init_charge_transfer_res 
+ * @param {*} PF_factor 
+ * @returns power fade
+ */
+function calc_pf_with_current_res(internal_res, charge_transfer_res, init_internal_res, init_charge_transfer_res, PF_factor) {
+    return (1 / (PF_factor - 1)) * (((internal_res + charge_transfer_res) / (init_charge_transfer_res + init_internal_res)) - 1);
+    //TODO: Needs to calc what internal and charge transfer res
+}
+
+//note when being used give it a CR (current rate) * Time
+/**
+ * @jordan use this
+ * Algorithm from Cordoba-Arenas et al. 2015
+ * Cycle aging algorithm
+ * calculates the resistance growth of power fade using SOC, total Ampere hours sent, and Temp conditions
+ * 
+ * @param {*} SOC_min = the minimum SOC 
+ * @param {*} Ah = Ampere-hour throughput ie the total current to the battery over over an hour
+ *                      (do we control this? Let's say the CR [ie C] is 1C == 2A)
+ * @param {*} tempurature = tempurature of the cells in the vehicle (Unit: Kelvin)
+ * @param {*} time_cd = time in charge depleting mode (Hybrid only. If full EV, set to 1)
+ * @param {*} time_cs = time in charge sustain mode (Hybrid only. If full EV, set to 0)
+ * @returns a percentage of power lose
+ */
+ function calc_pf_cycle_aging_with_SOC_and_temp(SOC_min, Ah, tempurature, time_cd = 1, time_cs = 0) {
+    var a_r = 3.2053 * Math.pow(10, 5),
+        B_r = 1.3573 * Math.pow(10, 9),
+        y_r = 3.6342 * Math.pow(10, 3),
+        c_r = 5.45,
+        d_r = 0.9179,
+        e_r = 1.8277,
+        E_aR = 51800,   //Unit: J/mol
+        R = 8.314;       //Unit: J/(mol*K)
+    
+        var ratio = time_cd / (time_cd + time_cs);
+    
+    return (a_r) + (B_r * Math.pow(SOC_min - 0.25, c_r)) + (y_r * Math.exp(d_r * (5 - ratio)) + e_r * (SOC_min - 0.25) * Math.exp(-E_aR / (R * tempurature)) * Ah);
+}
+
+
+
+
+
+/**
+ * bms means battery management system
+ */
+function bms() {
+    //TODO
+}
+
+//TODO: degrade battery functions (ie mkae each battery lose CF and PF)
--- public/scripts/fleet-capacity.js
+++ public/scripts/fleet-capacity.js
@@ -1,38 +1,53 @@
 // jQuery Document
 
+//
+// NOTE: THIS FILE IS INCLUDED IN AN HTML FILE SUCH THAT <battery-degredation.js>
+//       WILL BE PROCESSED FIRST IN A SHARED GLOBAL SCOPE !!!
+//
+
 
 //---------------------FILE GLOBAL VARIABLES------------------------
 
 // Variables for 3 user inputs. Names self-explanatory. MSOC is Minimum State of Charge
-var vehicle_type, msoc = .5, opt_in = .5, battery_capacity = 0, zip_code = 95050,
-	veh_pop, flt_cap, flt_profit, indiv_profit;
+var vehicle_type,
+  msoc = .5,
+  opt_in = .5,
+  battery_capacity = 0,
+  zip_code = 95050,
+  veh_pop,
+  flt_cap = [],
+  flt_profit,
+  indiv_profit,
+  temperature_in_Kelvin = 293, //TODO mk this variable using Arjun's work
+  PF = 1,                      //power fade (1 == 100%)
+  CF = 1;                      //capacity fade (1 == 100%) 
 
 const indiv_roi_field = $("#indiv-roi");
 const flt_roi_field = $("#flt-roi");
 const flt_cap_field = $("#fleet-capacity");
 
 var flt_tou_profit =[
-	{season: "Winter", revenue: 0, id: 1},
-	{season: "Summer", revenue: 0, id: 2}
+  {season: "Winter", revenue: 0, id: 1},
+  {season: "Summer", revenue: 0, id: 2}
 ]
 
 //PGE_TOU_D holds PGE's time of use rates and their on and off peak hours
 //in military time
-	let PGE_TOU_D = {
-		winter_peak : 0.29231,
-		winter_off_peak : 0.27493,
-		winter_peak_start : 1700,
-		winter_peak_end : 2000,
-		winter_month_start: "October",
-		winter_length: 243,
+let PGE_TOU_D = {
+  winter_peak : 0.29231,
+  winter_off_peak : 0.27493,
+  winter_peak_start : 1700,
+  winter_peak_end : 2000,
+  winter_month_start: "October",
+  winter_length: 243,
 
-		summer_peak : 0.36618,
-		summer_off_peak : 0.27122,
-		summer_peak_start : 1700,
-		summer_peak_end : 2000,
-		summer_month_start: "June",
-		summer_length : 122
-	}
+  summer_peak : 0.36618,
+  summer_off_peak : 0.27122,
+  summer_peak_start : 1700,
+  summer_peak_end : 2000,
+  summer_month_start: "June",
+  summer_length : 122
+}
 
 var percent_flt_avail = [ 98.72404738009172, 98.94407289973621, 99.14171692141903, 99.2652592375488, 99.3765154139245, 99.46787692544532, 99.55432398106454, 99.61723493763674, 99.6897083596079, 99.74791209637071, 99.80963884670155, 99.8363426974442, 99.87867807057278, 99.90940822253603, 99.94046403121568, 99.9705428697462, 99.7580962700464, 99.56391605161954, 99.15323332711625, 98.85356993774036, 98.15228300160756, 97.62090005595374, 96.51781194212785, 95.65973609963912, 93.88931816365135, 92.58302025880832, 89.98170401357103, 87.76611334630029, 83.61511520846483, 80.21235778423177, 75.05866261668102, 71.54956347197474, 67.21658243999764, 64.9553998324347, 61.50921164430003, 59.42474221310362, 55.51857871566923, 53.9576764689337, 51.14950899848729, 49.78047776800775, 45.994629624695385, 44.99995559226582, 43.165590519244915, 42.59965835649925, 40.507284348642195, 40.313311366307545, 39.43824216425528, 39.40709754010763, 38.651425932340466, 38.944132110048315, 38.91349077355303, 39.26591055098155, 38.76265250355997, 39.208802204991876, 39.33823594717245, 39.932441033930374, 39.780773819568424, 40.55166247753726, 41.07369019388408, 42.06294648275962, 42.78279585172533, 44.27957333049169, 45.70532803993127, 47.39240746168406, 49.06924349944776, 51.097877606364364, 53.02313050840919, 55.006054254409186, 57.0098200302565, 59.66110977887937, 62.2160939549232, 64.4942699220497, 66.4808054970854, 68.62555102596673, 70.68488567969007, 72.5788459318075, 74.46140819885203, 76.37763152830689, 78.29802918476284, 80.12842716687534, 81.94920347327688, 83.79008168062548, 85.59676593275482, 87.27312828802265, 88.91574076541167, 90.42213991988844, 91.81464803910247, 92.94902880285628, 94.01892953676811, 94.93698542538168, 95.8113441036891, 96.45741742381854, 97.05760275209529, 97.56278513465905, 98.004523667842, 98.36247960944878]
 
@@ -45,128 +60,213 @@ const zip_code_pop_95050 = 38699;
 //----------------------JQUERY UPDATES-----------------------
 //Retrieve zip code from map
 $("#zip").change(function(){
-	zip_code = $("#zip").val();
-	
-	calc_veh_pop();
-	calc_flt_cap();
-	flt_tou_profit[0].revenue = calc_revenue_pge_tou("Winter", 15);		//index 0 is winter
-	flt_tou_profit[1].revenue = calc_revenue_pge_tou("Summer", 15);		//index 1 is summer
-	flt_profit = Math.round(calc_annual_profit(flt_tou_profit[0].revenue, flt_tou_profit[1].revenue,
-		PGE_TOU_D.winter_length, PGE_TOU_D.summer_length));
-	indiv_profit = Math.round(calc_annual_profit(flt_tou_profit[0].revenue/veh_pop, flt_tou_profit[1].revenue/veh_pop,
-		PGE_TOU_D.winter_length, PGE_TOU_D.summer_length));
-	
-	flt_roi_field.text(flt_profit);
-	indiv_roi_field.text(indiv_profit);
-	flt_cap_field.text((battery_capacity*veh_pop).toFixed(0));
-	updatePS();
-	updateFA();
+  zip_code = $("#zip").val();
+  
+  calc_veh_pop();
+  calc_flt_cap();
+  flt_tou_profit[0].revenue = calc_revenue_pge_tou("Winter", 15);   //index 0 is winter
+  flt_tou_profit[1].revenue = calc_revenue_pge_tou("Summer", 15);   //index 1 is summer
+  flt_profit = Math.round(calc_annual_profit(flt_tou_profit[0].revenue, flt_tou_profit[1].revenue,
+    PGE_TOU_D.winter_length, PGE_TOU_D.summer_length));
+  indiv_profit = Math.round(calc_annual_profit(flt_tou_profit[0].revenue/veh_pop, flt_tou_profit[1].revenue/veh_pop,
+    PGE_TOU_D.winter_length, PGE_TOU_D.summer_length));
+  
+  flt_roi_field.text(flt_profit);
+  indiv_roi_field.text(indiv_profit);
+  flt_cap_field.text((battery_capacity*veh_pop).toFixed(0));
+  updatePS();
+  updateFA();
 });
 
 // When multiple choice is changed, update graphs. This function is called in app.js
 function updateGraphs(){
 
-	msoc = $("#msocText").val()/100; //is a percentage;
-	opt_in = $("#optinText").val()/100;
+  msoc = $("#msocText").val()/100; //is a percentage;
+  opt_in = $("#optinText").val()/100;
 
-	// Get id of selected vehicle
-	vehicle_type = $("#car-list .selected").attr("id");
-	//console.log(vehicle_type);
+  // Get id of selected vehicle
+  vehicle_type = $("#car-list .selected").attr("id");
+  //console.log(vehicle_type);
 
-	//console.log(data.batteryKWhCapacity);
-	battery_capacity = $("#car-list .selected div h6").attr("property");
+  //console.log(data.batteryKWhCapacity);
+  battery_capacity = $("#car-list .selected div h6").attr("property");
 
-	calc_veh_pop();
-	calc_flt_cap();
-	flt_tou_profit[0].revenue = calc_revenue_pge_tou("Winter", 15);		//index 0 is winter
-	flt_tou_profit[1].revenue = calc_revenue_pge_tou("Summer", 15);		//index 1 is summer
-	flt_profit = Math.round(calc_annual_profit(flt_tou_profit[0].revenue, flt_tou_profit[1].revenue,
-		PGE_TOU_D.winter_length, PGE_TOU_D.summer_length));
-	indiv_profit = Math.round(calc_annual_profit(flt_tou_profit[0].revenue/veh_pop, flt_tou_profit[1].revenue/veh_pop,
-		PGE_TOU_D.winter_length, PGE_TOU_D.summer_length));
+  calc_veh_pop();
+  calc_flt_cap();
+  flt_tou_profit[0].revenue = calc_revenue_pge_tou("Winter", 15);   //index 0 is winter
+  flt_tou_profit[1].revenue = calc_revenue_pge_tou("Summer", 15);   //index 1 is summer
+  flt_profit = Math.round(calc_annual_profit(flt_tou_profit[0].revenue, flt_tou_profit[1].revenue,
+    PGE_TOU_D.winter_length, PGE_TOU_D.summer_length));
+  indiv_profit = Math.round(calc_annual_profit(flt_tou_profit[0].revenue/veh_pop, flt_tou_profit[1].revenue/veh_pop,
+    PGE_TOU_D.winter_length, PGE_TOU_D.summer_length));
 
-	// Set the value of the HTML spans to a smaller number, expressed in larger units (e.g. Gigawatts vs Kilowatts)
-	let flt_val = battery_capacity * veh_pop;
-	let unit = "kWh";
+  // Set the value of the HTML spans to a smaller number, expressed in larger units (e.g. Gigawatts vs Kilowatts)
+  let flt_val = battery_capacity * veh_pop;
+  let unit = "kWh";
 
-	// Once for Megawatts
-	if(flt_val >= 1000) {
-		flt_val /= 1000;
-		unit = "MWh";
-	}
-	// Again for Gigawatts
-	if(flt_val >= 1000) {
-		flt_val /= 1000;
-		unit = "GWh";
+  // Once for Megawatts
+  if(flt_val >= 1000) {
+    flt_val /= 1000;
+    unit = "MWh";
+  }
+  // Again for Gigawatts
+  if(flt_val >= 1000) {
+    flt_val /= 1000;
+    unit = "GWh";
 
-		// Easter Egg -- if fleet capacity is approximately 1.21 GWh, replace unit with "Jigawatt-hours"
-		if(flt_val >= 1.21 && flt_val < 1.22)
-			unit = "Jigawatt-hours";
-	}
+    // Easter Egg -- if fleet capacity is approximately 1.21 GWh, replace unit with "Jigawatt-hours"
+    if(flt_val >= 1.21 && flt_val < 1.22)
+      unit = "Jigawatt-hours";
+  }
 
-	// Populate spans in HTML with the values, separated with commas (e.g. 1,000,000)
-	flt_roi_field.text(flt_profit.toLocaleString());
-	indiv_roi_field.text(indiv_profit.toLocaleString());
-	flt_cap_field.text((flt_val).toLocaleString() + " " +unit);
+  // Populate spans in HTML with the values, separated with commas (e.g. 1,000,000)
+  flt_roi_field.text(flt_profit.toLocaleString());
+  indiv_roi_field.text(indiv_profit.toLocaleString());
+  flt_cap_field.text((flt_val).toLocaleString() + " " +unit);
 
-	updatePS();
+  updatePS();
 
 }
 
 //----------------------UPDATE CALCULATIONS-----------------------
 
+
 //Determine the number of vehicles in the fleet
+//Assumes every person has 1 EV
 function calc_veh_pop() {
-	var pop = zip_code_pop_95050;//TO DO: use zip code -> populations
-	veh_pop = pop * opt_in;
-}
+  var pop = zip_code_pop_95050;//TO DO: use zip code -> populations
+  veh_pop = pop * opt_in;
 
-//update flt_cap
-function calc_flt_cap() {
-	flt_cap = veh_pop * (1 - msoc) * battery_capacity;
-	if (!(battery_capacity >= 0))	//checks for NaN
-		flt_cap = 0;
+  //could update so that we are using average veh in household and round at the end
 }
 
 
-//TO DO exclude holidays
+//update CF
+function update_CF() {
+  const avgEv_milesDrivenPerYr = 9435; // https://www.autoexpress.co.uk/tesla/352144/tesla-drivers-cover-higher-annual-mileages-owners-any-other-car
+  const avgEv_kWhPerMile       = 0.3;  // https://www.myev.com/research/comparisons/evs-with-the-best-mpge-ratings-for-2019/slide-4
+  const avgEv_amphrPerYear     = get_ampHr(avgEv_milesDrivenPerYr, avgEv_kWhPerMile);          // from <battery-degredation.js>
+  CF = calc_cf_cycle_aging_with_SOC_and_temp(msoc, avgEv_amphrPerYear, temperature_in_Kelvin); // from <battery-degredation.js>
+}
+
+
+//update flt_cap (NOW A JSON VALUE)
+//Assumes all vehicles in the fleet are the same
+/**
+ * Calculates the fleets capacity every day
+ * @param {*} years 
+ * @return - A fleet with an overall decaying battery in an array
+ */
+function calc_flt_cap(years = 5) {
+  update_CF(); // required to compute <batt_degrad_factor> below
+  const factory_new_flt_cap = veh_pop * (1 - msoc) * battery_capacity; // no battery degredation
+  const batt_degrad_factor = Math.min(CF, PF) / 100;
+  const batt_degrad_amount = factory_new_flt_cap * batt_degrad_factor;
+  flt_cap = [];
+  for(let i = 0; i < years; ++i) {
+    flt_cap.push({
+      'years': i,
+      'flt_cap': factory_new_flt_cap - (i * batt_degrad_amount);
+    });
+  }
+
+  ////////////////////////////////////
+  // MOTHBALLING ALL OF CHRIS' CODE //
+  ////////////////////////////////////
+
+  /*
+  var init_flt_cap = veh_pop * (1 - msoc) * battery_capacity * batt_degrad;
+  flt_cap[0] = veh_pop * (1 - msoc) * battery_capacity * batt_degrad;
+
+
+  //2. Fleet cap over x years.
+  //2a. pull charing station volts from MongoDB
+  var i,
+    volts = 120,    //charge station voltage rating   //TODO hook up to DB and data. currently defaulting to lvl 1 charger
+    amp_hours = 12,   //charge station amp_hour rating  //TODO hook up to DB and data. currently defaulting to lvl 1 charger
+    per_veh_kWh_avail = (1 - msoc) * battery_capacity,
+    temp = 295;     //arbitary hardcoded number     //TODO use map data from Arjun
+  
+  
+  
+  //perhaps make this a smaller interval?
+  //previously working at a daily level
+  for (i = 1; i < years * 365; i++){
+    //2b. Calculate CF and PF from a day's charging
+
+    //calculate the max amount of retrievable energy over peak hours
+    var veh_amp_hour = per_veh_kWh_avail * 1000 / volts;  //converts vehicle battery to an ampere-hour value using station voltage
+    var max_amps = Math.min(veh_amp_hour, amp_hours); //whichever is worse is the bottleneck
+    
+    //2c. use following function to store flt_cap of the day
+    
+    //update CF and PF
+    CF = 1 - calc_cf_calendar_aging(temp, i);
+    //PF -= calc_pf_with_SOC_and_temp(msoc, max_amps, temp);    //TODO find calendar aging power fade algorithm
+
+    //note: PF is not currently changing so PF >= CF
+    //flt_cap[i] = veh_pop * (1 - msoc) * battery_capacity * Math.min(CF, PF);
+    flt_cap.push(veh_pop * (1 - msoc) * battery_capacity * Math.min(CF, PF));
+
+  }
+  
+
+
+  if (!(battery_capacity >= 0)) //checks for NaN
+    flt_cap = 0;
+  
+  
+  console.log(flt_cap);
+  return flt_cap;
+*/
+}
+
+
+
+//TODO More the just PGE
+
+//TODO exclude holidays
+
+//TODO flt_cap is now an array
+
+//TODO MK BOTH INSTANCES OF <flt_cap[0]['flt_cap']> RELATIVE TO THE CURRENT YEAR INSTEAD OF ALWAYS THE FIRST (IE BY USING <0>) !!!
 /**
  * Calculates the money saved using PGE's TOU-D plan over one day
  * @param {*} season - Either Winter or Summer
  * @param {*} division_of_time - For example: hourly inputs would be 60
  */
 function calc_revenue_pge_tou(season, division_of_time) {
-	var revenue;
+  var revenue;
 
-	switch (season) {
-		case "Winter":
-			//create subarray of percent_flt_avail within the range of peak hours
-			var start_index = mil_time_to_minutes(PGE_TOU_D.winter_peak_start) / division_of_time;
-			var end_index = mil_time_to_minutes(PGE_TOU_D.winter_peak_end) / division_of_time;
-			var peak_avail = percent_flt_avail.slice(start_index, end_index);
+  switch (season) {
+    case "Winter":
+      //create subarray of percent_flt_avail within the range of peak hours
+      var start_index = mil_time_to_minutes(PGE_TOU_D.winter_peak_start) / division_of_time;
+      var end_index = mil_time_to_minutes(PGE_TOU_D.winter_peak_end) / division_of_time;
+      var peak_avail = percent_flt_avail.slice(start_index, end_index);
 
-			//All vehicles that can will participate in this V2G event. Thus the highest availability over the peak hours is 
-			//used to determine how much energy is discharged
-			//formula: [max availability over peak hours (percentage)] * fleet capacity
-			var discharged_energy = Math.max(...peak_avail)/100 * flt_cap;
-			//console.log(discharged_energy);
+      //All vehicles that can will participate in this V2G event. Thus the highest availability over the peak hours is 
+      //used to determine how much energy is discharged
+      //formula: [max availability over peak hours (percentage)] * fleet capacity
+      var discharged_energy = Math.max(...peak_avail)/100 * flt_cap[0]['flt_cap']; 
+      //console.log(discharged_energy);
 
-			revenue = discharged_energy * (PGE_TOU_D.winter_peak-PGE_TOU_D.winter_off_peak);
-			break;
-		case "Summer":
-			//create subarray of percent_flt_avail within the range of peak hours
-			var start_index = mil_time_to_minutes(PGE_TOU_D.summer_peak_start) / division_of_time;
-			var end_index = mil_time_to_minutes(PGE_TOU_D.summer_peak_end) / division_of_time;
-			var peak_avail = percent_flt_avail.slice(start_index, end_index);
+      revenue = discharged_energy * (PGE_TOU_D.winter_peak-PGE_TOU_D.winter_off_peak);
+      break;
+    case "Summer":
+      //create subarray of percent_flt_avail within the range of peak hours
+      var start_index = mil_time_to_minutes(PGE_TOU_D.summer_peak_start) / division_of_time;
+      var end_index = mil_time_to_minutes(PGE_TOU_D.summer_peak_end) / division_of_time;
+      var peak_avail = percent_flt_avail.slice(start_index, end_index);
 
-			//All vehicles that can will participate in this V2G event. Thus the highest availability over the peak hours is 
-			//used to determine how much energy is discharged
-			var discharged_energy = Math.max(...peak_avail)/100 * flt_cap;
-			revenue = discharged_energy * (PGE_TOU_D.summer_peak - PGE_TOU_D.summer_off_peak);
-			break;
-	}
+      //All vehicles that can will participate in this V2G event. Thus the highest availability over the peak hours is 
+      //used to determine how much energy is discharged
+      var discharged_energy = Math.max(...peak_avail)/100 * flt_cap[0]['flt_cap'];
+      revenue = discharged_energy * (PGE_TOU_D.summer_peak - PGE_TOU_D.summer_off_peak);
+      break;
+  }
 
-	return revenue;
+  return revenue;
 }
 
 /**
@@ -177,7 +277,7 @@ function calc_revenue_pge_tou(season, division_of_time) {
  * @param {*} summer_length - days of summer billing
  */
 function calc_annual_profit(winter_rev, summer_rev, winter_length, summer_length) {
-	return winter_rev*winter_length + summer_rev*summer_length;
+  return winter_rev*winter_length + summer_rev*summer_length;
 }
 
 /**
@@ -185,7 +285,7 @@ function calc_annual_profit(winter_rev, summer_rev, winter_length, summer_length
  * @param {*} mil_time - military time
  */
 function mil_time_to_minutes(mil_time) {
-	var hours = mil_time / 100;
-	var minutes = mil_time % 100;
-	return (minutes + hours * 60);
+  var hours = mil_time / 100;
+  var minutes = mil_time % 100;
+  return (minutes + hours * 60);
 }
